a2b8488eb48cfc573526d4f956d76854d98b9fc9
ClaireHappel <clairehap1@gmail.com>
2026-07-22 19:20:51 -0500

    update / minor color adjust

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable object-curly-newline */
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import ThemeProvider from '@mui/system/ThemeProvider';
@@ -15,7 +14,12 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NavBar />
-      <Box component="main" sx={{ pt: 16, pb: 8, backgroundColor: '#bfc9e2', minHeight: '100vh' }}>
+      <Box
+        component="main"
+        sx={{
+          pt: 16, pb: 8, backgroundColor: '#e1e5f8', minHeight: '100vh',
+        }}
+      >
         <Container maxWidth="md">
           <Posts />
         </Container>
